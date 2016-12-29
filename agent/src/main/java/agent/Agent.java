dbf8e27596bf5b60064f30dc16502466183dab2f
guojianwei7 <guojianwei@jd.com>
2016-12-29 17:48:20 +0800

    修改agent

--- agent/src/main/java/agent/Agent.java
+++ agent/src/main/java/agent/Agent.java
@@ -1,5 +1,8 @@
 package agent;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
@@ -17,32 +20,39 @@ public class Agent {
 
                 if ("other/Stuff".equals(s)) {
                     // ASM Code
-                    ClassReader reader = new ClassReader(bytes);
-                    ClassWriter writer = new ClassWriter(reader, 0);
-                    ClassPrinter visitor = new ClassPrinter(writer);
-                    reader.accept(visitor, 0);
-                    return writer.toByteArray();
-
+//                  return asm(bytes);
                     // Javassist
-//                    try {
-//                        ClassPool cp = ClassPool.getDefault();
-//                        CtClass cc = cp.get("other.Stuff");
-//                        CtMethod m = cc.getDeclaredMethod("run");
-//                        m.addLocalVariable("elapsedTime", CtClass.longType);
-//                        m.insertBefore("elapsedTime = System.currentTimeMillis();");
-//                        m.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
-//                                + "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
-//                        byte[] byteCode = cc.toBytecode();
-//                        cc.detach();
-//                        return byteCode;
-//                    } catch (Exception ex) {
-//                        ex.printStackTrace();
-//                    }
+                    return javassist();
                 }
 
                 return null;
             }
         });
+    }
+    private static byte[] asm(byte[] bytes){
+        ClassReader reader = new ClassReader(bytes);
+        ClassWriter writer = new ClassWriter(reader, 0);
+        ClassPrinter visitor = new ClassPrinter(writer);
+        reader.accept(visitor, 0);
+        return writer.toByteArray();
+    }
+
+    private static byte[] javassist(){
+        try {
+            ClassPool cp = ClassPool.getDefault();
+            CtClass cc = cp.get("other.Stuff");
+            CtMethod m = cc.getDeclaredMethod("run");
+            m.addLocalVariable("elapsedTime", CtClass.longType);
+            m.insertBefore("elapsedTime = System.currentTimeMillis();");
+            m.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
+                    + "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
+            byte[] byteCode = cc.toBytecode();
+            cc.detach();
+            return byteCode;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
 }
